0c3c58b956533be73de39c88d20ea646583a822c
Zolkyed <zolkyed@gmail.com>
2026-02-07 23:16:22 -0500

    session

--- src/main.py
+++ src/main.py
@@ -1,10 +1,22 @@
+from contextlib import asynccontextmanager
 from typing import Union
 
 from fastapi import FastAPI
+from api.db.session import init_db
 from api.events import router as event_router
 
-app = FastAPI()
-app.include_router(event_router, prefix="/api/events")
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    # before app startup up
+    init_db()
+    yield
+    # clean up
+
+
+app = FastAPI(lifespan=lifespan)
+app.include_router(event_router, prefix='/api/events')
+# /api/events
 
 
 @app.get("/")
@@ -13,12 +25,10 @@ def read_root():
 
 
 @app.get("/items/{item_id}")
-def read_item(item_id: int, q: str | None = None):
+def read_item(item_id: int, q: Union[str, None] = None):
     return {"item_id": item_id, "q": q}
 
 
 @app.get("/healthz")
 def read_api_health():
-    return {
-        "status": "ok"
-    }
+    return {"status": "ok"}
